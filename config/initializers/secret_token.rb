71082b800b6eff5151763fd89de1f1388a59d159
TJ Guthrie <tjguthrie4600@gmail.com>
2014-05-22 15:30:25 -0400

    Initial Build

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TtgtAws::Application.config.secret_token = 'b1289fb90adff7c9b9762f2da8c564c2ee35c6fa58c0c2e02eb9a7f14b024ee7928ce4114cb4f3dcf6f0f9499170f9de5d48673fe110c2e8af3b0e68c6fd0cbd'
