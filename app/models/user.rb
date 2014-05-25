71f168f0020cd74ad0b79e2e8d2de4e18d96f62a
TJ Guthrie <tjguthrie4600@gmail.com>
2014-05-25 00:08:11 -0400

    Adding sessions

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,11 +14,17 @@ class User < ActiveRecord::Base
   has_secure_password
 
   before_save { |user| user.email = email.downcase }
+  before_save :create_remember_token
 
   validates :name, presence: true, length: { maximum: 50 }
   VAILD_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VAILD_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
   validates :password, presence: true, length: { minimum: 6 }
   validates :password_confirmation, presence: true
+
+
+  def create_remember_token
+    self.remember_token = SecureRandom.urlsafe_base64
+  end
 
 end
