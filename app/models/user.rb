1f31330dccb87855dcced0a91946d093718dfd64
TJ Guthrie <tjguthrie4600@gmail.com>
2014-05-25 13:49:44 -0400

    Adding user authorization, completes users

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,9 +22,9 @@ class User < ActiveRecord::Base
   validates :password, presence: true, length: { minimum: 6 }
   validates :password_confirmation, presence: true
 
-
-  def create_remember_token
-    self.remember_token = SecureRandom.urlsafe_base64
-  end
+  private
+    def create_remember_token
+      self.remember_token = SecureRandom.urlsafe_base64
+    end
 
 end
