36a1697609df0c2d0f6d7daf15a005e043a0f06f
TJ Guthrie <tjguthrie4600@gmail.com>
2014-05-27 00:13:53 -0400

    Added security at the view level for instances, in the furture may want to implement at the controller level

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,7 +11,7 @@ class SessionsController < ApplicationController
     # Sign in the user
     if user && user.authenticate(params[:session][:password])
       sign_in user
-      redirect_back_or user
+      redirect_to user
 
     # Authentication failed
     else
